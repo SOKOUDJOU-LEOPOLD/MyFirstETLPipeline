788bfd9b0f20c0cf0f1059facc828d00e9ace19d
SOKOUDJOU Leopold <leopoldgatsing@gmail.com>
2025-08-29 18:31:36 -0400

    Initial commit

---
+++ main_database.py
@@ -0,0 +1,7 @@
+from utilities.helper_psql import init_logger, psql_connection_test, psql_connection, create_table
+
+# from utilities.get_data import get_selfdev_members
+
+if __name__ == "__main__":
+    init_logger()
+    psql_connection_test()
---
+++ utilities/helper_psql.py
@@ -0,0 +1,48 @@
+import psycopg2
+import logging
+
+# file name for logs: filename='./MyFirstETLPipeline/logs/app.log'
+def init_logger():
+    logging.basicConfig( level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info("Start of Program And Logger initialized.")
+
+def psql_connection_test():
+    """Connect to the PostgreSQL database server"""
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        logging.info('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="localhost", database="MyFirstETLPipelineDB", user ="postgres", password="postgres")
+
+        #create a cursor
+        cur = conn.cursor()
+
+        #execute a statement
+        logging.info('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        #display the PostgreSQL databse server version
+        db_version = cur.fetchone()
+        logging.info(db_version)
+
+        #close the communication with the PostgreSQL
+        cur.close()
+
+    except(Exception, psycopg2.DatabaseError) as error:
+        logging.error(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            logging.info('Database connection closed.')
+            
+
+def psql_connection():
+    pass
+
+def create_table(psql_conn):
+    pass
+
+def insert_member(psql_conn, member):
+    pass
+
